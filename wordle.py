c0507d904b0b9a8c9ddb18c5f4a5137e916ece58
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2023-01-30 15:31:10 +0100

    adding fules to repo

---
+++ wordle.py
@@ -0,0 +1,57 @@
+import random
+
+class Wordle():
+    def __init__(self, words_list_file):
+        self.words_list_file = words_list_file
+        self.words = self.get_words()
+        self.word = self.get_random_word()
+        self.guess_count = 0
+        
+
+    # get all the words from the file
+    def get_words(self):
+        with open(self.words_list_file, 'r') as f:
+            words = f.readlines()
+        return [word.strip() for word in words]
+    
+    def get_random_word(self):
+        return random.choice(self.words)
+    
+    def set_random_word(self):
+        self.word = self.get_random_word()
+
+    # check if the word is correct
+    def check_word(self, word):
+        return word == self.word
+            
+    # return the positions of the correct letters
+    def get_correct_letters(self, word):
+        correct_letters = []
+        for i in range(len(self.word)):
+            if self.word[i] == word[i]:
+                correct_letters.append(i)
+        return correct_letters
+
+    # return the positions of the incorrect letters
+    def get_incorrect_letters(self, word):
+        incorrect_letters = []
+        for i in range(len(self.word)):
+            if self.word[i] != word[i]:
+                incorrect_letters.append(i)
+        return incorrect_letters
+
+    # return the positions of the letters that are in the word but in the wrong position
+    def get_misplaced_letters(self, word):
+        misplaced_letters = []
+        for i in range(len(self.word)):
+            if self.word[i] != word[i] and word[i] in self.word:
+                misplaced_letters.append(i)
+        return misplaced_letters
+
+    # check if it's a valid word
+    def is_valid_word(self, word):
+        return word in self.words
+    
+    # add guess to guess count
+    def add_guess(self):
+        self.guess_count += 1
---
+++ wordleAPI.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from flask_restful import Resource, Api
+import ast
+
+from wordle import Wordle
+app = Flask(__name__)
+api = Api(app)
+game = Wordle('possible_words.txt')
+
+# api for checking if the word is correct and returning the positions of the correct letters and the misplaced letters
+class CheckWord(Resource):
+    def get(self, word):
+        game.add_guess()
+        if game.is_valid_word(word):
+            if game.check_word(word):
+                return {'correct': True}, 200
+            else:
+                return {'correct': False, 'correct_letters': game.get_correct_letters(word), 'misplaced_letters': game.get_misplaced_letters(word)}, 200
+        else:
+            return "Is not a valid word", 400
+
+# api for getting a new word
+class NewGame(Resource):
+    def get(self):
+        game.set_random_word()
+        game.guess_count = 0
+
+        return {'word': game.word}, 200
+
+# api for checking if the word is valid
+class IsValidWord(Resource):
+    def get(self, word):
+        return {'valid': game.is_valid_word(word)}, 200
+
+# api for getting the word
+class GetWord(Resource):
+    def get(self):
+        return {'word': game.word}, 200
+
+# api for getting the number of guesses
+class GetGuessCount(Resource):
+    def get(self):
+        return {'guess_count': game.guess_count}, 200
+
+api.add_resource(CheckWord, '/guess/<string:word>')
+api.add_resource(NewGame, '/new_game')
+api.add_resource(IsValidWord, '/is_valid_word/<string:word>')
+api.add_resource(GetWord, '/get_word')
+api.add_resource(GetGuessCount, '/get_guess_count')
+
+app.run(debug=True)
+        
